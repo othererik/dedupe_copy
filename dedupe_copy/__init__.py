e52dde9f778d9cd53f9c83ee79aa53b9e3e702ff
eschweller <eschweller@nasuni.com>
2026-01-15 08:56:17 -0500

    version bump, 1.2.1

--- dedupe_copy/__init__.py
+++ dedupe_copy/__init__.py
@@ -1,6 +1,6 @@
 """DedupeCopy - Find duplicates and copy/restructure file layouts."""
 
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __author__ = "Erik Schweller"
 __email__ = "othererik@gmail.com"
 
